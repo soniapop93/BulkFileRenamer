73085b21d4044c42e8e31273fca8b39067d84ea7
soniapop93 <popsonia85@gmail.com>
2023-05-20 15:41:49 +0300

    first commit

---
+++ BulkFileRenamer/FileHandler/File.cs
@@ -0,0 +1,19 @@
+﻿namespace BulkFileRenamer.FileHandler
+{
+    public class File
+    {
+        public string oldFileName { get; set; }
+        public string oldFileExtension { get; set; }
+
+        public string newFileName { get; set; }
+        public string newFileExtension { get; set; }
+
+        public File(string oldFileName, string oldFileExtension, string newFileName, string newFileExtension)
+        {
+            this.oldFileName = oldFileName;
+            this.oldFileExtension = oldFileExtension;
+            this.newFileName = newFileName;
+            this.newFileExtension = newFileExtension;
+        }
+    }
+}
